0a246f30059604c2c1ac56c277957c9201b8c33a
haphamtrungtin <haphamtrungtin@gmail.com>
2024-09-25 19:56:25 +0700

    Feature: Mail sending

--- src/app/components/introduce/introduce.component.ts
+++ src/app/components/introduce/introduce.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
+
 interface Item {
   desc: string;
   image: string;
@@ -29,10 +31,36 @@ export class IntroduceComponent {
   lastName: string = '';
   firstName: string = '';
   message: string = '';
+  isSend = false;
+  notification = '';
 
   onSubmit() {
-    const mailtoLink = `mailto:dongsug503@gmail.com,pomielvietnam@gmail.com?subject=Contact%20Form%20Submission&body=Email:%20${encodeURIComponent(this.email)}%0D%0A%0D%0AHọ:%20${encodeURIComponent(this.lastName)}%0D%0A%0D%0ATên:%20${encodeURIComponent(this.firstName)}%0D%0A%0D%0ALời%20nhắn:%20${encodeURIComponent(this.message)}`;
+    const templateParams = {
+      email: this.email,
+      message: this.message,
+      lastName: this.lastName,
+      firstName: this.firstName,
+      toEmails: ' pomielvietnam@gmail.com'
+    };
 
-    window.location.href = mailtoLink;
+    const serviceID = 'service_by8nou9';
+    const templateID = 'template_v3l260s';
+    const publicKey = 'aqgqBD-t5wjmIVhFb';
+
+    emailjs
+    .send(serviceID, templateID, templateParams, {
+      publicKey,
+    })
+    .then(
+      () => {
+        this.notification = 'Gửi thành công, cảm ơn bạn đã gửi thông tin!';
+        this.isSend = true;
+      },
+      (error) => {
+        this.isSend = true;
+        this.notification = 'Gửi thất bại, vui lòng thử lại!';
+        throw new Error(error);
+      },
+    );
   }
 }
